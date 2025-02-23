e4e30fe6520744011d0dad7f4ba9b40ebf53d2c3
JCamiloRamirezTabares <juancaram21@gmail.com>
2025-02-23 15:05:37 -0500

    feat: init project

---
+++ src/main/java/co/edu/icesi/ClassManagementApplication.java
@@ -0,0 +1,13 @@
+package co.edu.icesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassManagementApplication.class, args);
+	}
+
+}
