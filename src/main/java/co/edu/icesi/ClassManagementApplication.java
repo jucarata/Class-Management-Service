ced469f11a99d271787b5abb117cb7008b7f62fe
JCamiloRamirezTabares <juancaram21@gmail.com>
2025-02-23 19:06:08 -0500

    config: added support for rest template - added the bean annotation for the rest template

--- src/main/java/co/edu/icesi/ClassManagementApplication.java
+++ src/main/java/co/edu/icesi/ClassManagementApplication.java
@@ -2,6 +2,8 @@ package co.edu.icesi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ClassManagementApplication {
@@ -10,4 +12,8 @@ public class ClassManagementApplication {
 		SpringApplication.run(ClassManagementApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
