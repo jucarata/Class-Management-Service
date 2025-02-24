d8d2c06498d53b6112cac1955ecf2dc25e523f8c
JCamiloRamirezTabares <juancaram21@gmail.com>
2025-02-23 19:07:15 -0500

    feat: add class service layer

--- src/main/java/co/edu/icesi/service/ClassServiceImpl.java
+++ src/main/java/co/edu/icesi/service/ClassServiceImpl.java
@@ -2,25 +2,34 @@ package co.edu.icesi.service;
 
 import co.edu.icesi.dto.ClassDTO;
 import co.edu.icesi.dto.ClassResponseDTO;
+import co.edu.icesi.dto.TrainerDTO;
 import co.edu.icesi.mappers.ClassMapper;
 import co.edu.icesi.persistence.model.Classes;
 import co.edu.icesi.persistence.repository.ClassRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ClassServiceImpl implements ClassService {
 
-    @Autowired
     private final ClassRepository classRepository;
-
     private final ClassMapper classMapper;
 
-    public ClassServiceImpl(ClassRepository classRepository, ClassMapper classMapper) {
+    private final RestTemplate restTemplate;
+
+    @Value("trainer.service.url")
+    private String trainerServiceUrl;
+
+    @Autowired
+    public ClassServiceImpl(ClassRepository classRepository, ClassMapper classMapper, RestTemplate restTemplate) {
         this.classRepository = classRepository;
         this.classMapper = classMapper;
+        this.restTemplate = restTemplate;
     }
 
     @Override
@@ -34,7 +43,16 @@ public class ClassServiceImpl implements ClassService {
     public List<ClassResponseDTO> getClasses() {
         List<Classes> allClasses = classRepository.findAll();
 
+        return allClasses.stream()
+                .map(classes -> CompletableFuture.supplyAsync(() -> fetchTrainer(classes)))
+                .map(CompletableFuture::join)
+                .toList();
+    }
 
-        return ;
+    private ClassResponseDTO fetchTrainer(Classes classes) {
+        String url = trainerServiceUrl + "/" + classes.getTrainerID().getId();
+        TrainerDTO trainerDTO = restTemplate.getForObject(url, TrainerDTO.class);
+
+        return classMapper.toDTO(classes, trainerDTO);
     }
 }
