1b7c26d8a1f09b2c037ad31626ff915edea4b3b5
JCamiloRamirezTabares <juancaram21@gmail.com>
2025-02-23 19:06:50 -0500

    refactor: remove some unnecesary annotations

--- src/main/java/co/edu/icesi/persistence/model/TrainerID.java
+++ src/main/java/co/edu/icesi/persistence/model/TrainerID.java
@@ -1,16 +1,15 @@
 package co.edu.icesi.persistence.model;
 
 import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Embeddable
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class TrainerID implements Serializable {
     private Long id;
 }
