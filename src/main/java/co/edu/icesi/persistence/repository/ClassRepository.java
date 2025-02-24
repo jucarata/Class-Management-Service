a65c83129d8523d28b6115cdcb59673a72af2d84
JCamiloRamirezTabares <juancaram21@gmail.com>
2025-02-23 19:25:46 -0500

    feat: added repository annotation

--- src/main/java/co/edu/icesi/persistence/repository/ClassRepository.java
+++ src/main/java/co/edu/icesi/persistence/repository/ClassRepository.java
@@ -2,6 +2,8 @@ package co.edu.icesi.persistence.repository;
 
 import co.edu.icesi.persistence.model.Classes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClassRepository extends JpaRepository<Classes,Integer> {
 }
